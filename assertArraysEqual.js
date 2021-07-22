0700d94398f9c894824d9cb552bf2f8f436a0897
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-07-22 12:23:16 +0000

    created function to console log equality message for two arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function (firstArray, secondArray) {
+  for (let i = 0; i <= firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
