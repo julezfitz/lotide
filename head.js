909da5bd1ad056edbdd160db7c4a35c8999ff179
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-07-23 20:05:25 +0000

    completed middle function and testing

--- head.js
+++ head.js
@@ -2,7 +2,7 @@ const head = function(array) {
   return array[0];
 };
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
---
+++ middle.js
@@ -0,0 +1,48 @@
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i <= firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+const middle = function(array) {
+  const midIndex = array.length / 2;
+  let midArray = [];
+  console.log(midIndex);
+  if (array.length >= 2 && array.length % 2 !== 0) {
+    console.log('large array with uneven number');
+    midArray.push(array[midIndex - 0.5]);
+  } else if (array.length >= 2 && array.length % 2 === 0) {
+    console.log('large array with even number');
+    console.log(midIndex - 0.5);
+    midArray.push(array[midIndex - 1]);
+    midArray.push(array[midIndex]);
+  } else {
+    console.log('small array');
+  }
+  return midArray;
+};
+
+//Test Assertions
+middle([1,2,3,4,5]);
+middle([1,2,3,4]);
+middle([1]);
+
+const testArray = [3,4,5,6,7,8,9,10];
+const testArray2 = [3];
+const testArray3 = [4,5,6,7,8,9,1];
+
+assertArraysEqual(middle(testArray),[6,7]);
+assertArraysEqual(middle(testArray2),[]);
+assertArraysEqual(middle(testArray3),[7]);
+assertArraysEqual(testArray, [3,4,5,6,7,8,9,10]); //Does not modify original array.
