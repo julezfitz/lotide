6d5448d3f06417361ee96287cf5ebc1150f694db
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-07-25 11:33:53 +0000

    created count letters function

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (results[letter]) {
+        results[letter] += 1;
+      } else {
+        results[letter] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const result1 = countLetters("apple I ate");
+
+assertEqual(result1["p"], 2);
+assertEqual(result1["z"], undefined);
+assertEqual(result1["a"], 2);
+assertEqual(result1["l"], 1);
+assertEqual(result1[" "], undefined);
