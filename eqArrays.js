28f8ec356f3a026f3f61701c148d97ffdb6e0002
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-05 15:11:46 +0000

    refractored eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -7,15 +7,19 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i <= firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
+  if (firstArray.length === secondArray.length) {
+    for (let i = 0; i <= firstArray.length; i++) {
+      if (Array.isArray(firstArray[i])) {
+        if (eqArrays(firstArray[i], secondArray[i]) === false) {
+          return false;
+        }
+      } else if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
+  return false;
 };
 
 //Test assertions
@@ -23,3 +27,6 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
