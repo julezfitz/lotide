e601b6128920bac0ca4d86184239e44ea9fde41b
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-09 21:11:09 -0230

    added mocha chai testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual.js');
-
 const eqArrays = function(firstArray, secondArray) {
   if (firstArray.length === secondArray.length) {
     for (let i = 0; i <= firstArray.length; i++) {
@@ -15,14 +13,5 @@ const eqArrays = function(firstArray, secondArray) {
   }
   return false;
 };
-
-//Test assertions
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
 
 module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,5 +1,3 @@
-const eqArrays = require('./eqArrays.js');
-const assertArraysEqual = require('./assertArraysEqual.js');
 
 const middle = function(array) {
   const midIndex = array.length / 2;
@@ -19,16 +17,4 @@ const middle = function(array) {
   return midArray;
 };
 
-//Test Assertions
-middle([1,2,3,4,5]);
-middle([1,2,3,4]);
-middle([1]);
-
-const testArray = [3,4,5,6,7,8,9,10];
-const testArray2 = [3];
-const testArray3 = [4,5,6,7,8,9,1];
-
-assertArraysEqual(middle(testArray),[6,7]);
-assertArraysEqual(middle(testArray2),[]);
-assertArraysEqual(middle(testArray3),[7]);
-assertArraysEqual(testArray, [3,4,5,6,7,8,9,10]); //Does not modify original array.
+module.exports = middle;
