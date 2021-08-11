6628bb4d4458cfbd59b7f9d44001cb3f3f5b0397
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-11 20:15:44 +0000

    moved eqArrays tests to test file

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,11 +1,28 @@
 const eqArrays = require('../eqArrays');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 //Test assertions
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+
+describe("#eqArrays", () => {
+  it("returns true for ['1', '2', '3'] is equal to ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  });
+  it("returns false for ['1', '2', '3'] is equal to ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it("returns true for [1, 2, 3] is equal to [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for [1, 2, 3] is equal to [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("returns true for [[2, 3], [4]] is equal to [[2, 3], [4]]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("returns false for [[2, 3], [4]] is equal to [[2, 3], [4, 5]]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("returns false for [[2, 3], [4]] is equal to [[2, 3], 4]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+});
